6a21d373c2ea2658a2c8f01f0dcc9a07bb63c2c0
liang chen <liang@michaels.com>
2022-05-05 17:20:12 -0500

    Remove legacy files

--- archived/src/main/java/TomcatServer/TomcatV1.java
+++
@@ -1,7 +0,0 @@
-package TomcatServer;
-
-public class TomcatV1 {
-    public static void main(String[] args) {
-
-    }
-}
--- archived/src/main/java/TomcatServer/indexServlet/IndexServlet.java
+++
@@ -1,4 +0,0 @@
-package TomcatServer.indexServlet;
-
-public class IndexServlet  {
-}
